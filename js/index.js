60564187d818fe4a3a37ca5555cb6118f92ee443
mh52mio <56579879+mh52mio@users.noreply.github.com>
2019-10-19 01:17:50 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.nav-device-item').eq(1).click(function(){
+        alert("敬请期待~");
+    });
+    $('.nav-device-item').eq(2).click(function(){
+        alert("敬请期待~");
+    });
+});
+
+
+function test(){
+	var a = 1;
+	var b = 3;
+	alert(a+b);
+	alert("111111");
+}
